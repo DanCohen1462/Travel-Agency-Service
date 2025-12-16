5a2625da733159aadaf0d68a3819a6153eff40ea
dancohen <danco6@ac.sce.ac.il>
2025-12-17 00:30:00 +0200

    fix problem with mytrips + add mytips to worker + front change

--- TravelAgency/Controllers/UsersController.cs
+++ TravelAgency/Controllers/UsersController.cs
@@ -15,7 +15,8 @@ namespace TravelAgency.Controllers
         public UsersController(IConfiguration config)
         {
             _connectionString = config.GetConnectionString("DefaultConnection")
-                                ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+                                ?? throw new InvalidOperationException(
+                                    "Connection string 'DefaultConnection' not found.");
         }
 
         // ---------- Dashboard (GET) - דף נחיתה ללקוח רגיל (המלבנים) ----------
@@ -52,7 +53,7 @@ namespace TravelAgency.Controllers
             {
                 return RedirectToAction("Dashboard", "Admin");
             }
-            
+
             // אם עובד (type=2), שלח אותו ללוח הבקרה של עובד
             if (userType == 2)
             {
@@ -61,11 +62,11 @@ namespace TravelAgency.Controllers
 
             // אם לקוח (type=3), נציג את דף המלבנים
             ViewData["Title"] = "Customer Dashboard";
-            HttpContext.Session.SetString("UserType", userType.ToString()); 
+            HttpContext.Session.SetString("UserType", userType.ToString());
 
             return View();
         }
-        
+
         // ---------- Profile (GET) ----------
         [HttpGet]
         public IActionResult Profile()
@@ -101,15 +102,15 @@ namespace TravelAgency.Controllers
                             return RedirectToAction("Dashboard", "Admin");
                         }
 
-                        model.Id          = reader.GetInt32(0);
-                        model.Username    = reader.GetString(1);
-                        model.FirstName   = reader.GetString(2);
-                        model.LastName    = reader.GetString(3);
-                        model.BirthDate   = reader.IsDBNull(4) ? (DateTime?)null : reader.GetDateTime(4);
-                        model.Gender      = reader.IsDBNull(5) ? "" : reader.GetString(5);
+                        model.Id = reader.GetInt32(0);
+                        model.Username = reader.GetString(1);
+                        model.FirstName = reader.GetString(2);
+                        model.LastName = reader.GetString(3);
+                        model.BirthDate = reader.IsDBNull(4) ? (DateTime?)null : reader.GetDateTime(4);
+                        model.Gender = reader.IsDBNull(5) ? "" : reader.GetString(5);
                         model.PhoneNumber = reader.IsDBNull(6) ? "" : reader.GetString(6);
-                        model.Email       = reader.GetString(7);
-                        model.Type        = type;
+                        model.Email = reader.GetString(7);
+                        model.Type = type;
                     }
                 }
             }
@@ -170,14 +171,14 @@ namespace TravelAgency.Controllers
 
                 using (var cmd = new SqlCommand(sql, conn))
                 {
-                    cmd.Parameters.AddWithValue("@Username",   model.Username);
-                    cmd.Parameters.AddWithValue("@FirstName",  model.FirstName);
-                    cmd.Parameters.AddWithValue("@LastName",   model.LastName);
-                    cmd.Parameters.AddWithValue("@BirthDate",  (object?)model.BirthDate ?? DBNull.Value);
-                    cmd.Parameters.AddWithValue("@Gender",     (object?)model.Gender ?? DBNull.Value);
+                    cmd.Parameters.AddWithValue("@Username", model.Username);
+                    cmd.Parameters.AddWithValue("@FirstName", model.FirstName);
+                    cmd.Parameters.AddWithValue("@LastName", model.LastName);
+                    cmd.Parameters.AddWithValue("@BirthDate", (object?)model.BirthDate ?? DBNull.Value);
+                    cmd.Parameters.AddWithValue("@Gender", (object?)model.Gender ?? DBNull.Value);
                     cmd.Parameters.AddWithValue("@PhoneNumber", (object?)model.PhoneNumber ?? DBNull.Value);
-                    cmd.Parameters.AddWithValue("@Email",      model.Email);
-                    cmd.Parameters.AddWithValue("@Id",         userId);
+                    cmd.Parameters.AddWithValue("@Email", model.Email);
+                    cmd.Parameters.AddWithValue("@Id", userId);
 
                     cmd.ExecuteNonQuery();
                 }
@@ -192,7 +193,6 @@ namespace TravelAgency.Controllers
             return View(model);
         }
 
-       // ---------- MyTrips (מחובר לדאטה בייס) ----------
         public IActionResult MyTrips()
         {
             // 1. בדיקת התחברות
@@ -207,19 +207,19 @@ namespace TravelAgency.Controllers
             {
                 conn.Open();
 
-                // שאילתה שמחברת בין טבלת ההזמנות (h) לטבלת החבילות (p)
-                // ולוקחת את המחיר וכמות האנשים מההזמנה, ואת היעד והתאריכים מהחבילה
+                // 🟢 תיקון: מחקתי את p.Image מהשאילתה כי העמודה לא קיימת
                 string sql = @"
-                    SELECT 
-                        h.Id AS ReservationId,
-                        h.numPersons,
-                        h.sum,
-                        p.destination,
-                        p.startDate,
-                        p.endDate
-                    FROM HistoryReservation h
-                    INNER JOIN Package p ON h.PackageId = p.Id
-                    WHERE h.UserId = @UserId AND h.inactive = 0";
+            SELECT 
+                h.Id AS ReservationId,
+                h.numPersons,
+                h.sum,
+                p.destination,
+                p.startDate,
+                p.endDate
+            FROM HistoryReservation h
+            INNER JOIN Package p ON h.PackageId = p.Id
+            WHERE h.UserId = @UserId AND h.inactive = 0
+            ORDER BY p.startDate DESC";
 
                 using (var cmd = new SqlCommand(sql, conn))
                 {
@@ -231,19 +231,22 @@ namespace TravelAgency.Controllers
                         {
                             var trip = new UserTripViewModel();
 
-                            // מילוי הנתונים מהדאטה-בייס
                             trip.ReservationId = reader.GetInt32(reader.GetOrdinal("ReservationId"));
-                            
-                            // בדיקת NULLים ליתר ביטחון
-                            trip.NumPersons = reader.IsDBNull(reader.GetOrdinal("numPersons")) ? 1 : reader.GetInt32(reader.GetOrdinal("numPersons"));
-                            trip.TotalPrice = reader.IsDBNull(reader.GetOrdinal("sum")) ? 0 : reader.GetInt32(reader.GetOrdinal("sum"));
-                            
-                            trip.Destination = reader.GetString(reader.GetOrdinal("destination"));
-                            trip.StartDate = reader.GetDateTime(reader.GetOrdinal("startDate"));
-                            trip.EndDate = reader.GetDateTime(reader.GetOrdinal("endDate"));
 
-                            // תמונה דיפולטיבית (כי אין בטבלה הזו תמונה)
-                            trip.ImageUrl = "/images/default.jpg";
+                            // בדיקת NULLים
+                            trip.NumPersons = reader.IsDBNull(reader.GetOrdinal("numPersons"))
+                                ? 1
+                                : reader.GetInt32(reader.GetOrdinal("numPersons"));
+
+                            var sumObj = reader["sum"];
+                            trip.TotalPrice = (sumObj == DBNull.Value) ? 0 : Convert.ToInt32(sumObj);
+
+                            trip.Destination = reader["destination"].ToString();
+                            trip.StartDate = Convert.ToDateTime(reader["startDate"]);
+                            trip.EndDate = Convert.ToDateTime(reader["endDate"]);
+
+                            // 🟢 תיקון: מכיוון שאין עמודת תמונה בדאטה בייס, ניתן תמונה קבועה ויפה
+                            trip.ImageUrl = "https://images.unsplash.com/photo-1469854523086-cc02fe5d8800";
 
                             myTrips.Add(trip);
                         }
@@ -251,10 +254,8 @@ namespace TravelAgency.Controllers
                 }
             }
 
-            ViewData["Title"] = "MyTrips";
             return View(myTrips);
         }
-        
     }
-    
 }
+    
