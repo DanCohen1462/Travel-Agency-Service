b52bbdd452fa2814e2cf2f25b43402527ec53523
dancohen <danco6@ac.sce.ac.il>
2025-12-16 23:18:34 +0200

    profile front change and add employeedashboard

--- TravelAgency/Controllers/EmplolyeeController.cs
+++ TravelAgency/Controllers/EmplolyeeController.cs
@@ -5,16 +5,11 @@ namespace TravelAgency.Controllers
 {
     public class EmployeeController : Controller
     {
-        // פעולה שמציגה את דף הבית של העובד
-        public IActionResult EmployeeHome()
-        {
-            // בדיקת אבטחה קטנה: רק עובד יכול לראות את הדף הזה
-            var role = HttpContext.Session.GetString("Role");
-            if (role != "Employee")
-            {
-                return RedirectToAction("Index", "Home");
-            }
+        // ... שאר הקוד בקונטרולר ...
 
+        // הפעולה שמציגה את דף הבית של העובד
+        public IActionResult EmployeeDashboard()
+        {
             return View();
         }
     }
