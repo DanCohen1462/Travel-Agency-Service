9ec22c6fb0387fd95872859473cc6169a97490ae
NadavDavidpur <nadavdavidpur@gmail.com>
2026-01-07 18:20:07 +0200

    update valdition

--- TravelAgency/Models/PackageView.cs
+++ TravelAgency/Models/PackageView.cs
@@ -12,7 +12,6 @@ public class PackageView
     [Required(ErrorMessage = "Start date is required")]
     public DateTime StartDate { get; set; }
     [Required(ErrorMessage = "End date is required")]
-    [DateGreaterThan("StartDate", ErrorMessage = "End date must be after start date")]
     public DateTime EndDate { get; set; }
     [Required(ErrorMessage = "Price is required")]
     [Range(1, int.MaxValue, ErrorMessage = "Price must be greater than 0")]
@@ -77,7 +76,7 @@ public class DateGreaterThanAttribute1 : ValidationAttribute
         if (comparisonValue == null)
             return ValidationResult.Success;
 
-        // ⚠ דרישה חדשה: StartDate חייב להיות לפחות יום אחד אחרי היום
+        
       
 
         // בדיקה רגילה: EndDate > StartDate
